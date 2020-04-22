70e13d3b7b5544883753d638e3bcf893483b1ae1
InternalHell <internallhell@gmail.com>
2020-04-22 17:51:57 +0300

    factorial

--- fib.py
+++ fib.py
@@ -1,7 +1,18 @@
 def fib(n):
-    fib = [0,1] + [0] * (n - 1)
-    for i in range(2,n+1):
+    assert n >= 0, 'Error'
+    fib = [None] * (n + 1)
+    fib[:2] = [0,1]
+    for i in range(2, n+1):
         fib[i] = fib[i-1] + fib[i-2]
     return fib[n]
 a = int(input('Напиши любое число для просчёта числа фибоначи:\n-->'))
-print(f'Твоё число: {a}\nИндекс числа фибоначи: {fib(a)}')
+print(f'Твоё число: {a}\nЧисло фибоначи: {fib(a)}')
+
+
+def fac(n):
+    assert n >= 0, 'Error'
+    list = [1] + [None] * n
+    for i in range(1, n+1):
+        list[i] = list[i-1] * i
+    return list[n]
+print('\n',fac(12))
