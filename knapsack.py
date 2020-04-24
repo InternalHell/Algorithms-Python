13ed63c53064700a2d847fb980be8d9cd36a85b5
InternalHell <internallhell@gmail.com>
2020-04-24 20:56:31 +0300

    radix_sort and knapsack,class

--- knapsack.py
+++ knapsack.py
@@ -1,27 +1,22 @@
 
-def knapsack_wit1hout_repeats(capacity, weight_list):
+def knapsack_without_repeats(capacity, weight_list):
     """Time complexity: O(capacity * n), where n = len(item_list)
     Gold cost per unit is constant => item cost in proportional to it's weight
     Let the cost per unit = 1
     """
     cost = 1
     previous = [0] * (capacity + 1)
-
     for i in range(1, len(weight_list) + 1):
         current = [0]
-
         for w in range(1, capacity + 1):
             current.append(previous[w])
             if weight_list[i - 1] <= w:
                 current[w] = max(current[w], previous[w - weight_list[i - 1]] + cost * weight_list[i - 1])
         previous = current
-
     return previous[capacity] // cost
 
-
 def main():
-    capacity = (int(i) for i in input().split())
+    capacity = int(input())
     weight_list = [int(i) for i in input().split()]
-
     print(knapsack_wit1hout_repeats(capacity, weight_list))
 main()
---
+++ radix_sort.py
@@ -0,0 +1,43 @@
+
+
+def radix_sort(list, radix=10): # radix - система счисления
+    """
+    :param arr: Iterable of elements to sort.
+    :param radix: Base of input numbers
+    :return: Sorted list of input.
+    Time complexity: O(d * (n + b))
+    Space complexity: O(n + k)
+    where, k is the range of input.
+    """
+    max = False
+    digit = 1
+    while not max:
+        max = True # Значит цикл идёт 1-ин раз
+        # Создаём список в списке с индексами, от 0 до 9
+        a = [[] for i in range(radix)]
+        # Добавляем в созданные списки числа, если же tmp при делении будет выдовать 0, то цикл после продолжения закончится
+        for i in list:
+            tmp = i // digit # какой разряд смотрим, если делит на 1, то 1-ый разряд
+            a[tmp % radix].append(i)
+            if max and tmp > 0:
+                max = False
+        # Обновляем список с отсортированными разрядами
+        add = 0
+        for b in range(radix):
+            a_copy_index = a[b]
+            for i in a_copy_index:
+                list[add] = i
+                add += 1
+        # После окончания итерации с 1-ым разрядом, переходим ко 2-ому, * на 10
+        digit *= radix
+    return list
+
+def main():
+    """
+    Test radix sort
+    """
+    test = [int(i) for i in input().split()]
+    print('Sorted array:', radix_sort(test))
+    
+if __name__ == '__main__':
+    main()
